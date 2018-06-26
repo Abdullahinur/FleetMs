f49ebc9e5b99e1d7a9b8ef1408eea6be45b45947
abdullahinur abdullahi <aabdullahinur@gmail.com>
2018-06-26 23:23:43 +0300

    reset master branch to start

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/abdullahinur/Documents/my-django-apps/FleetMs/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
